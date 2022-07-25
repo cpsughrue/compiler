83ee584aef90cbf37fe93c3caca9642bed30b14b
Connor Sughrue <55301806+cpsughrue@users.noreply.github.com>
2022-07-25 17:02:32 -0400

    Update build_main.sh

--- asm/build_main.sh
+++ asm/build_main.sh
@@ -1,13 +1,15 @@
 
 # -f        specifies the output file format
 # elf64     x86_64 64 bit object files
+
+# assemble to object file
 nasm -f elf64 -o print_int.o print_int.asm
 nasm -f elf64 -o main.o main.asm
 
-# -m EMULATOR
+# link object files
 ld -o print_int print_int.o main.o
 
-
+# run executable
 ./print_int
 
 # clean up
