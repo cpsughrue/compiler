aab057874c2d6e5092584b7dfd31d1062a503481
cpsughrue <cpsughrue@gmail.com>
2022-07-25 22:17:27 -0400

    cleaned up header files

--- include/parser.h
+++ include/parser.h
@@ -1,6 +1,8 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "scanner.h"
+
 typedef enum
 {
     ADD_EXPR,
--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "scanner.h"
 #include "parser.h"
 
 int main(int argc, char *argv[])
